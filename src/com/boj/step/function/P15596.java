4f2576012a1d4b06d8b9fa482321ca4e42143c0f
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-12-27 11:25:57 +0900

    []: solve P15596

---
+++ src/com/boj/step/function/P15596.java
@@ -0,0 +1,12 @@
+package com.boj.step.function;
+
+
+public class P15596 {
+    long sum(int[] a) {
+        long ans = 0;
+        for (int i = 0; i < a.length; i++) {
+            ans += a[i];
+        }
+        return ans;
+    }
+}
