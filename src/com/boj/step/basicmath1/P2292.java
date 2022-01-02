98ce34f4559fe4a0a32f64b195eb4e1b8c0c7c89
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2022-01-02 11:46:45 +0900

    [feat]: solve P2292

---
+++ src/com/boj/step/basicmath1/P2292.java
@@ -0,0 +1,26 @@
+package com.boj.step.basicmath1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2292 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        Integer input = Integer.parseInt(bf.readLine());
+
+        int value = 1;
+        int count = 1;
+
+        while (true) {
+            if (value >= input) {
+                System.out.println(count);
+                break;
+            } else {
+                value = value + (count * 6);
+                count++;
+            }
+        }
+
+    }
+}
