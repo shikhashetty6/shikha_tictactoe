307ab71fece0f1de16d0cec6f02e0eda44a2aaa9
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2022-01-26 13:23:26 +0900

    [feat]: solve P10926

---
+++ src/com/boj/step/inputoutputncalculation/P10926.java
@@ -0,0 +1,16 @@
+package com.boj.step.inputoutputncalculation;
+
+import java.io.*;
+
+public class P10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+        bw.write(s+"??!");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
