37f1341d7335961eeebe86f509d85fc0d8a1892e
friendshipkim97 <rlawjddn5980@naver.com>
2022-04-04 12:32:06 +0900

    [feat] feat PathSearchDFS

---
+++ src/com/lecture/dfsbfsbasic/PathSearchDFS.java
@@ -0,0 +1,41 @@
+package com.lecture.dfsbfsbasic;
+
+import java.util.Scanner;
+
+public class PathSearchDFS {
+
+    static int n, m, answer = 0;
+    static int[][] graph;
+    static int[] ch;
+
+    public void DFS(int v) {
+        if(v==n) answer++;
+        else{
+            for (int i = 1; i <= n; i++) {
+                if (graph[v][i] == 1 && ch[i] == 0) {
+                    ch[i]=1;
+                    DFS(i);
+                    ch[i]=0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PathSearchDFS pathSearchDFS = new PathSearchDFS();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        graph = new int[n+1][n+1];
+        ch = new int[n+1];
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a][b] = 1;
+        }
+        ch[1] = 1;
+        pathSearchDFS.DFS(1);
+        System.out.println(answer);
+
+    }
+}
